d906a503cab13e16298b959415ecd75f5d3853c7
konick <konick.gevurah@gmail.com>
2024-02-18 20:07:43 +0300

    В классе для работы с польской нотацией добавлен метод calcInPolishNotation. Этот класс переводит строку с польской нотацией в Integer

---
+++ src/main/java/tasks/polishNotation/PolishNotation.java
@@ -0,0 +1,34 @@
+package tasks.polishNotation;
+
+import java.util.Deque;
+import java.util.LinkedList;
+public class PolishNotation {
+    public static int calcInPolishNotation(String polishNotation) {
+        Deque<Integer> stack = new LinkedList<>();
+
+        for (String value: polishNotation.split(" ")) {
+            if (Operand.contains(value)) {
+                if (stack.size() < 2) {
+                    throw new RuntimeException("Количество значений в стеке меньше 2. Неверная польская нотация");
+                }
+                Integer b = stack.pollFirst();
+                Integer a = stack.pollFirst();
+
+                Operand operand = Operand.valueOfSign(value);
+                //System.out.println(a + value + b + "=" + operand.calculate(a, b));
+                stack.addFirst(operand.calculate(a, b));
+            } else {
+                stack.addFirst(Integer.parseInt(value));
+            }
+        }
+        if (stack.size() != 1) {
+            throw new RuntimeException("В стеке осталось больше одного значения");
+        }
+        return stack.poll();
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(Operand.DEL.calculate(12, 4));
+    }
+}
