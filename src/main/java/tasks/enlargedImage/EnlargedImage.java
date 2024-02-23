bab9805bd0aed15a3f3bd744d0bb6989a2d88064
konick <konick.gevurah@gmail.com>
2024-02-24 01:42:06 +0300

    Решена задача enlargeArray. "Расширение двумерного массива"

---
+++ src/main/java/tasks/enlargedImage/EnlargedImage.java
@@ -0,0 +1,32 @@
+package tasks.enlargedImage;
+
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+public class EnlargedImage {
+    public static String[][] enlargeArrayImage(String[][] matrix) {
+        return enlargeArrayImage(matrix, 2);
+    }
+
+    public static String[][] enlargeArrayImage(String[][] arr, int coef) {
+
+        return Arrays.stream(arr)
+                .flatMap(line -> {
+                    String[][] repeatElemets = new String[coef][];
+                    Arrays.fill(repeatElemets, repeatElement(line, coef));
+                    return Stream.of(repeatElemets);
+                })
+                .toArray(String[][]::new);
+    }
+
+    private static String[] repeatElement(String[] line, int coef) {
+        return Arrays.stream(line)
+                .flatMap(string -> {
+                    String[] lines = new String[coef];
+                    Arrays.fill(lines, string);
+                    return Stream.of(lines);
+                })
+                .toArray(String[]::new);
+    }
+}
+// END
---
+++ src/test/java/tasks/enlargedImage/EnlargedImageTest.java
@@ -0,0 +1,52 @@
+package tasks.enlargedImage;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+public class EnlargedImageTest {
+    @DisplayName("Проверка функции enlargeArrayImage")
+    @ParameterizedTest(name = "{displayName}: {arguments}")
+    @MethodSource("provideEnlargeArrayImage")
+    public void testEnlargeArrayImage(String[][] matrix, String[][] expected) {
+        String[][] actual = EnlargedImage.enlargeArrayImage(matrix);
+
+        Assertions.assertTrue(Arrays.deepEquals(actual, expected), Arrays.deepToString(actual) + "\n" + Arrays.deepToString(expected));
+    }
+
+    private static Stream<Arguments> provideEnlargeArrayImage() {
+        return Stream.of(
+                Arguments.of(
+                        new String[][]{
+                                {"*"}
+                        },
+                        new String[][]{
+                                {"*", "*"},
+                                {"*", "*"}}
+                ),
+                Arguments.of(
+                        new String[][]{
+                                {"*", "*", "*", "*"},
+                                {"*", " ", " ", "*"},
+                                {"*", " ", " ", "*"},
+                                {"*", "*", "*", "*"}},
+                        new String[][]{
+                                {"*", "*", "*", "*", "*", "*", "*", "*"},
+                                {"*", "*", "*", "*", "*", "*", "*", "*"},
+                                {"*", "*", " ", " ", " ", " ", "*", "*"},
+                                {"*", "*", " ", " ", " ", " ", "*", "*"},
+                                {"*", "*", " ", " ", " ", " ", "*", "*"},
+                                {"*", "*", " ", " ", " ", " ", "*", "*"},
+                                {"*", "*", "*", "*", "*", "*", "*", "*"},
+                                {"*", "*", "*", "*", "*", "*", "*", "*"}
+                        }
+                )
+        );
+    }
+}
+// END
